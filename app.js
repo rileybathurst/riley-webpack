2e6dae593cdce8c860af263d94ed6c866a2c948b
Riley Bathurst <riley@rileybathurst.com>
2019-06-29 10:11:59 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+
+import {MDCTextField} from '@material/textfield';
+import {MDCTextFieldIcon} from '@material/textfield/icon';
+
+import {MDCRipple} from '@material/ripple/index';
+
+const textFieldElements = [].slice.call(document.querySelectorAll('.mdc-text-field'));
+textFieldElements.forEach((textFieldEl) => {
+  new MDCTextField(textFieldEl);
+});
+
+const icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field-icon'));
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
